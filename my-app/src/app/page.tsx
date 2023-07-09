0beeeaa2cc2b9c38cabe50f9e87b3822b246cd78
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-07-10 00:42:23 +0800

    lol

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from "next/image";
+import axios from "axios";
+
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:80").then((res) => setData(res.data));
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold text-center">{data || ""}</h1>
+        <h1 className="text-6xl font-bold text-center">{data || ""}</h1>
+      </div>
+    </main>
+  );
+}
